b3c04440152bf08218c668a55edf1437eb4d518a
knockknockyoo <iheaven0129@gmail.com>
2025-02-13 11:13:25 -0500

    removed percentage

--- src/common/s3util.py
+++ src/common/s3util.py
@@ -60,26 +60,19 @@ class S3Bucket:
 
     def put_file_obj(self, file_size, key, data, md5_base64):
         # Initialize the progress bar
-        progress = create_progress_bar(file_size)
+        #progress = create_progress_bar(file_size)
         chunk_size = 1024 * 1024 if file_size >= 1024 * 1024 else file_size #chunk data for display progress for small metadata file < 4,500,000,000 bytes
-        try:
-            # Upload the file in chunks
-            for chunk in iter(lambda: data.read(chunk_size), b''):
-                self.bucket.put_object(Key=key,
-                                        Body=chunk,
-                                        ContentMD5=md5_base64,
-                                        ACL= BUCKET_OWNER_ACL,
-                                        )
-                # Update the progress bar
-                progress.update(len(chunk))
-        finally:
-            # Close the progress bar
-            progress.close()
+        for chunk in iter(lambda: data.read(chunk_size), b''):
+            self.bucket.put_object(Key=key,
+                                   Body=chunk,
+                                   ContentMD5=md5_base64,
+                                   ACL= BUCKET_OWNER_ACL,
+                                   )
 
     def upload_file_obj(self, file_size, key, data, config=None, extra_args={'ACL': BUCKET_OWNER_ACL}):
         self.bucket.upload_fileobj(
             data, key, ExtraArgs=extra_args, Config=config,
-            Callback=ProgressPercentage(file_size)
+
         )
 
     def get_object_size(self, key):
@@ -110,7 +103,7 @@ class S3Bucket:
         try:
             file_size, msg = self.get_object_size(key)
             self.bucket.download_file(key, local_file_path,
-                                      Callback=ProgressPercentage(file_size))
+                                      )
             return True, None
         except ClientError as ce:
             msg = None
