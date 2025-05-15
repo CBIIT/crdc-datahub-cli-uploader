4823d4312f031739b9b60586d86416192faca626
vshand11 <105606628+vshand11@users.noreply.github.com>
2025-05-15 17:09:21 -0400

    implemented

--- src/process_manifest.py
+++ src/process_manifest.py
@@ -103,7 +103,7 @@ def add_file_id(file_id_name, file_name_name, final_manifest_path, file_infos, m
     return True
 
 # insert file node ID into relationship data fiels in children's metadata file.
-def insert_file_id_2_children(configs, manifest_rows, final_file_path_list, manifest_s3_url):
+def insert_file_id_2_children(log, configs, manifest_rows, final_file_path_list, manifest_s3_url):
      # check if any tsv files in the dir of manifest file
     manifest_file = configs.get(PRE_MANIFEST)
     is_s3 = configs.get(FROM_S3, False)
@@ -116,7 +116,7 @@ def insert_file_id_2_children(configs, manifest_rows, final_file_path_list, mani
             download_meatadata_in_s3(manifest_s3_url, s3_bucket)
             
         tsv_files = [os.path.join(dir, f) for f in os.listdir(dir) if os.path.isfile(os.path.join(dir, f)) 
-                    and (f.endswith('.tsv') or f.endswith('.txt')) and f != manifest_file] 
+                    and (f.endswith('.tsv') or f.endswith('.txt')) and f not in manifest_file and not "-final." in f] 
         file_type = manifest_rows[0].get(NODE_TYPE_NAME) 
         if file_type:
             file_id_to_check = f"{file_type}.{configs.get(FILE_ID_FIELD)}"
@@ -148,6 +148,8 @@ def insert_file_id_2_children(configs, manifest_rows, final_file_path_list, mani
                             final_file_path = file.replace(file_ext, f'-final{file_ext}')
                             df.to_csv(final_file_path, sep ='\t', index=False)
                             final_file_path_list.append(final_file_path)
+    except Exception as e:
+        log.exception(f"Failed to insert file id into children tsv files. Error: {e}")
     finally:
         if s3_bucket:
             s3_bucket = None
