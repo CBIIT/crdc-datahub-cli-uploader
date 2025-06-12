ce82712990549674297986f0e01557a0e572f445
vshand11 <105606628+vshand11@users.noreply.github.com>
2025-06-12 12:17:30 -0400

    updated

--- src/process_manifest.py
+++ src/process_manifest.py
@@ -42,8 +42,7 @@ def process_manifest_file(log, configs, has_file_id, file_infos, manifest_rows, 
     file_name_name = configs[FILE_NAME_FIELD]
     if not has_file_id:
         manifest_columns.append(file_id_name)
-    if hasSubFolder:
-        manifest_columns.append(SUBFOLDER_FILE_NAME)
+    manifest_columns.append(SUBFOLDER_FILE_NAME)
     result = None
     newBatch = None
     final_file_path_list = []
